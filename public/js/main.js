74f86f5daba7fd565dd5edc9598568f15251c422
Nadya <nadyarq@bu.edu>
2016-04-09 15:35:30 -0400

    added new files

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,47 @@
 
+$(document).ready(function() {
+	getWeather();
+})
+
 function searchWeather() {
-  document.getElementById("demo").innerHTML = "NEWWW PARAGRAPH #javascript #swag";
+  var searchQuery = $(.'search').val();
+  getWeather(searchQueary);
 }
 
+function getWeather(searchQuery){
+	var url = 'http://api.openweathermap.org/data/2.5/weather?';
+	var params = {
+		APPID: apiKey.
+		units: 'imperial'
+	};
+	if(searchQuery)
+	{
+		params.q = searchQuery;
+	}
+	else
+	{
+		params.id = 4930956
+	}
+	
+	$.ajax(url + $.param(params), {
+		success: function (data) {
+			var countryName = getCountryName(data.sys.country)
+			$('.city').text('${data.name}, ${countryName}');
+			$('.temp').text('${data.main.temp} °F');
+			$('.summary').html(parseSummary(data));
+		},
+		error: function(error){
+			$('.error-message').text('An error occurred');
+		}
+	});
+}
+
+function parseSummary(data){
+	if(data.weather){
+		var weatherItems = data.weather.map(function(weatherItem){
+			var description = weatherItem.description;
+			var iconSrc = 'http://openweathermap.org/img/w/' + weatherItem.icon + '.png';
+		});
+		return weatherItems.join('');
+	}
+}
